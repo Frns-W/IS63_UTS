4bede70c65488915a43e37b79ce8df529e8ca067
Frns-W <fwhennardy@gmail.com>
2026-05-21 17:10:29 +0700

    layout master

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Sistem Kafe - SB Admin 2</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar (Navigasi Samping) -->
+        <ul class="navbar-nav bg-gradient-dark sidebar sidebar-dark accordion" id="accordionSidebar">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-coffee"></i>
+                </div>
+                <div class="sidebar-brand-survey mx-3">Cafe Kita</div>
+            </a>
+
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item {{ Request::is('/') ? 'active' : '' }}">
+                <a class="nav-link" href="/">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span></a>
+            </li>
+
+            <hr class="sidebar-divider">
+
+            <div class="sidebar-heading">Manajemen Data</div>
+
+            <!-- Nav Item - Menu -->
+            <li class="nav-item {{ Request::is('menu') ? 'active' : '' }}">
+                <a class="nav-link" href="/menu">
+                    <i class="fas fa-fw fa-list"></i>
+                    <span>Daftar Menu</span></a>
+            </li>
+
+            <!-- Nav Item - Orders -->
+            <li class="nav-item {{ Request::is('orders') ? 'active' : '' }}">
+                <a class="nav-link" href="/orders">
+                    <i class="fas fa-fw fa-shopping-cart"></i>
+                    <span>Riwayat Order</span></a>
+            </li>
+
+            <hr class="sidebar-divider d-none d-md-block">
+        </ul>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar (Navbar Atas Palsu untuk merapikan layout) -->
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow"></nav>
+
+                <!-- Begin Page Content (Tempat Konten Anak Dimasukkan!) -->
+                <div class="container-fluid px-4 mx-auto" style="max-width: 1400px;">
+                    
+                    {{-- DI SINI TEMPAT KONTEN DASHBOARD/MENU/ORDERS DIKUNCI --}}
+                    @yield('main-content')
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Cafe Kita 2026</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\CafeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route untuk Halaman Dashboard Kafe
+Route::get('/', [CafeController::class, 'index']);
+
+// Route untuk Halaman Daftar Menu
+Route::get('/menu', [CafeController::class, 'menu']);
+
+// Route untuk Halaman Riwayat Transaksi
+Route::get('/orders', [CafeController::class, 'orderHistory']);
