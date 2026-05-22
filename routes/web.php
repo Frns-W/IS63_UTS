6e82e691fae6df3318425db64aec5a60c885241a
Frns-W <fwhennardy@gmail.com>
2026-05-22 11:49:35 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\CafeController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\MenuController;
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
 // Route untuk Halaman Dashboard Kafe
@@ -15,14 +17,14 @@ Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->
 Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
 Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
 
-// Route untuk Halaman Daftar Menu
-Route::get('/menu', [CafeController::class, 'menu'])->name('menu.index');
-Route::get('/menu/create', [CafeController::class, 'createMenu'])->name('menu.create');
-Route::post('/menu', [CafeController::class, 'storeMenu'])->name('menu.store');
-Route::get('/menu/{menu}', [CafeController::class, 'showMenu'])->name('menu.show');
-Route::get('/menu/{menu}/edit', [CafeController::class, 'editMenu'])->name('menu.edit');
-Route::put('/menu/{menu}', [CafeController::class, 'updateMenu'])->name('menu.update');
-Route::delete('/menu/{menu}', [CafeController::class, 'destroyMenu'])->name('menu.destroy');
+// Route untuk Halaman Daftar Menu (MenuController)
+Route::get('/menu', [MenuController::class, 'index'])->name('menu.index');
+Route::get('/menu/create', [MenuController::class, 'create'])->name('menu.create');
+Route::post('/menu', [MenuController::class, 'store'])->name('menu.store');
+Route::get('/menu/{menu}', [MenuController::class, 'show'])->name('menu.show');
+Route::get('/menu/{menu}/edit', [MenuController::class, 'edit'])->name('menu.edit');
+Route::put('/menu/{menu}', [MenuController::class, 'update'])->name('menu.update');
+Route::delete('/menu/{menu}', [MenuController::class, 'destroy'])->name('menu.destroy');
 
-// Route untuk Halaman Riwayat Transaksi
-Route::get('/orders', [CafeController::class, 'orderHistory'])->name('orders.index');
+// Route untuk Halaman Riwayat Transaksi (OrderController)
+Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
