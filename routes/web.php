ec3a88cfa8f292be70ec925d19f07695abeb285b
Frns-W <fwhennardy@gmail.com>
2026-07-25 14:03:04 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,47 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CafeController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\MenuController;
 use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
-// Route untuk Halaman Dashboard Kafe
-Route::get('/', [CafeController::class, 'index']);
+// ========== ROUTE AUTENTIKASI ==========
 
-// Route untuk Halaman Data Kategori
-Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
-Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
-Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
-Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
-Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
-Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+// Halaman login (hanya bisa diakses jika belum login)
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+});
 
-// Route untuk Halaman Daftar Menu (MenuController)
-Route::get('/menu', [MenuController::class, 'index'])->name('menu.index');
-Route::get('/menu/create', [MenuController::class, 'create'])->name('menu.create');
-Route::post('/menu', [MenuController::class, 'store'])->name('menu.store');
-Route::get('/menu/{menu}', [MenuController::class, 'show'])->name('menu.show');
-Route::get('/menu/{menu}/edit', [MenuController::class, 'edit'])->name('menu.edit');
-Route::put('/menu/{menu}', [MenuController::class, 'update'])->name('menu.update');
-Route::delete('/menu/{menu}', [MenuController::class, 'destroy'])->name('menu.destroy');
+// Proses logout (hanya untuk yang sudah login)
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-// Route untuk Halaman Riwayat Transaksi (OrderController)
-Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
+// ========== ROUTE YANG DILINDUNGI (WAJIB LOGIN) ==========
+
+Route::middleware('auth')->group(function () {
+
+    // Route untuk Halaman Dashboard Kafe
+    Route::get('/', [CafeController::class, 'index']);
+
+    // Route untuk Halaman Data Kategori
+    Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+    Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
+    Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
+    Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+    Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
+    Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+
+    // Route untuk Halaman Daftar Menu (MenuController)
+    Route::get('/menu', [MenuController::class, 'index'])->name('menu.index');
+    Route::get('/menu/create', [MenuController::class, 'create'])->name('menu.create');
+    Route::post('/menu', [MenuController::class, 'store'])->name('menu.store');
+    Route::get('/menu/{menu}', [MenuController::class, 'show'])->name('menu.show');
+    Route::get('/menu/{menu}/edit', [MenuController::class, 'edit'])->name('menu.edit');
+    Route::put('/menu/{menu}', [MenuController::class, 'update'])->name('menu.update');
+    Route::delete('/menu/{menu}', [MenuController::class, 'destroy'])->name('menu.destroy');
+
+    // Route untuk Halaman Riwayat Transaksi (OrderController)
+    Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
+});
